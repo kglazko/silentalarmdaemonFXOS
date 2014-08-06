12e9964522fbe55bec20ebd381ae20462ecc3e2a
yglazko <yglazko@yglazko.local>
2014-08-06 13:37:23 -0700

    Removing trash, spaces, debug, etc.

--- test-alarm_daemon/js/silentalarm.js
+++ test-alarm_daemon/js/silentalarm.js
@@ -2,32 +2,24 @@
 
 window.onload = function() {
   var button = document.getElementById('start');
-  var tempH;
-  console.log("HI?");
   button.addEventListener('click', function() {
-  	console.log("HELLO?");
-
-	var hours = document.getElementById('hr').value;
-	var repeat = document.getElementById('repeat').value;
-    var currentTime = new Date();
-    // Let's schedule the daemon now
-    for (var i = 0; i<repeat; i++)
+  var hours = document.getElementById('hr').value;
+  var repeat = document.getElementById('repeat').value;
+  var currentTime = new Date();
+  // Let's schedule the daemon now
+  for (var i = 0; i<repeat; i++)
     {
 	currentTime.setHours(currentTime.getHours()+ hours);
-	
-
 	// This is arbitrary data pass to the alarm
 	var data    = {
   	foo: "bar"
 	}
 
-	// The "honorTimezone" string is what make the alarm honoring it
-	var request = navigator.mozAlarms.add(currentTime, "honorTimezone", data);
-
-	request.onsuccess = function () {
-  console.log("The alarm has been scheduled");
+  // The "honorTimezone" string is what make the alarm honoring it
+  var request = navigator.mozAlarms.add(currentTime, "honorTimezone", data);
+  request.onsuccess = function () {
+  	console.log("The alarm has been scheduled");
 	};
-
 	request.onerror = function () { 
   	console.log("An error occurred: " + this.error.name);
 	};
