6f50d2b8916095694bf957cf1db2d0599c1e8b0f
yglazko <yglazko@yglazko.local>
2014-08-06 13:49:50 -0700

    Changing misleading variable name

--- test-alarm_daemon/js/silentalarm.js
+++ test-alarm_daemon/js/silentalarm.js
@@ -2,23 +2,31 @@
 
 window.onload = function() {
     var button = document.getElementById('start');
+    var tempH;
+    console.log("HI?");
     button.addEventListener('click', function() {
+        console.log("HELLO?");
+
         var hours = document.getElementById('hr').value;
         var repeat = document.getElementById('repeat').value;
-        var currentTime = new Date();
+        var alarmTime = new Date();
         // Let's schedule the daemon now
         for (var i = 0; i < repeat; i++) {
-            currentTime.setHours(currentTime.getHours() + hours);
+            alarmTime.setHours(currentTime.getHours() + hours);
+
+
             // This is arbitrary data pass to the alarm
             var data = {
                 foo: "bar"
             }
 
             // The "honorTimezone" string is what make the alarm honoring it
-            var request = navigator.mozAlarms.add(currentTime, "honorTimezone", data);
+            var request = navigator.mozAlarms.add(alarmTime, "honorTimezone", data);
+
             request.onsuccess = function() {
                 console.log("The alarm has been scheduled");
             };
+
             request.onerror = function() {
                 console.log("An error occurred: " + this.error.name);
             };
